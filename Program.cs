dda7780c8d5e3b35f880b13aa5b4b4d4b648db75
NazarDaineko <115698927+NazarDaineko@users.noreply.github.com>
2022-11-23 19:05:03 +0500

    task2 complite

--- Program.cs
+++ Program.cs
@@ -4,11 +4,15 @@ int age = 14;
 string Email = "IvanovNikita@gmail.com";
 double matPoints = 8.3;
 double progPoints = 10.0;
-double physPoints = 5.6;
+double physPoints = 6.6;
 string pattern = "Имя: {0} \nВозраст: {1} \nПочта: {2} \nБаллы по программированию: {3} \nБаллы по математике: {4} \nБаллы по физике: {5}";
+double pointsSum = matPoints + progPoints + physPoints;
+double arithmeticMeanPoints = pointsSum / 3; //Не знаю, почему, но пишет 8.299999999999999
 
-Console.WriteLine("Нажмите Enter, чтобы вызвать блок 1.");
+Console.WriteLine("Нажмите Enter, чтобы начать");
 Console.ReadLine();
+
+#region Task1
 Console.WriteLine(pattern,
                   fullName,
                   age,
@@ -17,4 +21,10 @@ Console.WriteLine(pattern,
                   matPoints,
                   physPoints);
 Console.ReadLine();
+#endregion
 
+#region Task2
+Console.WriteLine("Сумма баллов: " + pointsSum);
+Console.WriteLine("Средний балл: " + arithmeticMeanPoints);
+Console.ReadLine();
+#endregion
