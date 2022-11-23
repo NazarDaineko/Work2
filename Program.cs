0f0bbc1fdbf2d6fd43235fc32c906eabd42ff3fe
NazarDaineko <115698927+NazarDaineko@users.noreply.github.com>
2022-11-23 18:12:31 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//
+string fullName = "Иванов Никита Иванович";
+int age = 14;
+string Email = "IvanovNikita@gmail.com";
+double matPoints = 8.3;
+double progPoints = 10.0;
+double physPoints = 5.6;
+string pattern = "Имя: {0} \nВозраст: {1} \nПочта: {2} \nБаллы по программированию: {3} \nБаллы по математике: {4} \nБаллы по физике: {5}";
+
+Console.WriteLine("Нажмите Enter, чтобы вызвать блок 1.");
+Console.ReadLine();
+Console.WriteLine(pattern,
+                  fullName,
+                  age,
+                  Email,
+                  progPoints,
+                  matPoints,
+                  physPoints);
+Console.ReadLine();
